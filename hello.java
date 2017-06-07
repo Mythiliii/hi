2eebf14d85ced713107169e16ab62ad32ccd85bc
Mythiliii <smythilieee2410@gmail.com>
2017-06-07 15:09:16 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,16 @@
+package positive;
+import java.io.*;
+import java.util.Scanner;
+public class hello {
+		public static void main(String args[])
+		{
+			int i=1;
+			int n=5;
+			if(i>=n)
+			{
+				System.out.println("hello");
+				i++;
+			}
+		}
+	}
+
