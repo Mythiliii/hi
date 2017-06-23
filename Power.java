ecbba1fc81dfb09545c016e7af857191592a9421
Mythiliii <smythilieee2410@gmail.com>
2017-06-23 14:45:36 +0530

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Power {
+	public static void main(String[] args)
+	{
+		 int a = 2;
+
+		    int b = 3;
+
+		    System.out.println(Math.pow(a, b));
+
+	}
+
+}
