2b9c8f7143045ee057e36a5771148f775e67a772
Mythiliii <smythilieee2410@gmail.com>
2017-07-14 12:21:15 +0530

    Add files via upload

---
+++ ReverseInt.java
@@ -0,0 +1,13 @@
+
+public class ReverseInt {
+		public void reverse(int n) {
+			   int result = 0;
+			   int rem;
+			   while (n > 0) {
+			       rem = n % 10;
+			       n = n / 10;
+			       result = result * 10 + rem;
+			   }
+			}
+}
+
