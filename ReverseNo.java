bf04271275beb78b50649085b127fd67f4cf6e08
Mythiliii <smythilieee2410@gmail.com>
2017-07-14 11:59:17 +0530

    Add files via upload

--- ReverseNo.java
+++ ReverseNo.java
@@ -2,21 +2,13 @@ import java.util.*;
 public class ReverseNo {
 		public static void main(String[] args) 
 		{
+			String str; 
+			System.out.println("Enter the string: ");
 			Scanner s=new Scanner(System.in);
-			String a;
-			a=s.next();
-			int j=a.length();
-			j--;
-			char str1[]=a.toCharArray();
-			char temp;
-			for(int i=0;i<str1.length/2;i++){
-				temp=str1[i];
-				str1[i]=str1[j];
-				str1[j]=temp;
-				j--;
-			}
-			a=new String(str1);
-			System.out.println(a);
-	        s.close();
-		}
+			str =s.next();
+		      String reverse = new StringBuffer(str).reverse().toString();
+		      System.out.println("\n String before reverse: "+str);
+		      System.out.println("\n String after reverse: "+reverse);
+		      s.close();
+		   }
 }
